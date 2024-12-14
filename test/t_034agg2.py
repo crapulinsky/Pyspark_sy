afc381b707efc0e7e1381b643e97bf5962d0897e
SMOG <SOMG_33@outlook.com>
2024-12-14 15:22:42 +0800

    00

---
+++ test/t_034agg2.py
@@ -0,0 +1,50 @@
+import os
+import time
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count,expr
+'''
+-------------------------------------------
+    Description: TODO:
+    SourceFile: t_034agg2
+    Author: SMOG
+    Data: 2024/12/14
+-------------------------------------------
+'''
+if __name__ == '__main__':
+    #程序开始运行
+    start_time = time.time()
+    # 设置环境变量
+    os.environ['JAVA_HOME'] = r'E:\environment\java\jdk-1.8'  # 设置 Java 路径
+    os.environ['SPARK_HOME'] = r'D:\Python\miniconda3\Lib\site-packages\pyspark'  # 设置 Spark 路径
+    os.environ['HADOOP_HOME'] = r'E:\Hadoop_local\hadoop-3.3.6'  # 如果需要 Hadoop，可选
+    #配置base环境python解释器的路径
+    os.environ['PYSPARK_PYTHON'] = r'D:\Python\miniconda3\python.exe'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = r'D:\Python\miniconda3\python.exe'
+
+    # 创建 SparkSession
+    spark = SparkSession.builder \
+        .appName("{NAME}") \
+        .master("local[*]") \
+        .getOrCreate()
+    print(spark)
+
+    df = spark.read.format("csv") \
+        .option("header", "true") \
+        .option("inferSchema", "true") \
+        .load("file:///D:/XXX/PycharmProjects/Pyspark_sy/data/retail-data/by-day/2010-12-01.csv") \
+        .coalesce(5)
+
+    df.groupBy("InvoiceNo").agg(
+        count("Quantity").alias("quan"),
+        expr("count(Quantity)")
+    ).show()
+
+
+
+
+
+    #程序结束
+    spark.stop()
+    end_time = time.time()
+    #运行时间
+    print("运行时间:",end_time - start_time)
